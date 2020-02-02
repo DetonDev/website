5957927838c8a51165a98e66fe81caa178f46c1d
detondev <50115345+DetonDev@users.noreply.github.com>
2020-02-02 18:12:02 -0500

    Progress torwards improved type function

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-var i = 0;
-var txt = "Samuel Giles"; /* The text */
-/* var speed = 50; The speed/duration of the effect in milliseconds */
+/* var txt = "Samuel Giles"; The text */
 
-function type() {
+function type(txt) {
+
+  let i = 0;
+  txt.toString();
+
   if (i < txt.length) {
+
+    let speed = Math.floor(Math.random() * (70 - 30 + 1) + 30);
     document.getElementById("title").innerHTML += txt.charAt(i);
     i++;
-    speed = Math.floor(Math.random() * (70 - 30 + 1) + 30);
     setTimeout(type, speed);
-  }
-}
 
-type();
+  };
+
+};
+
+type("Samuel Giles");
