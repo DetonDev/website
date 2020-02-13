ca6d1dd2148485ff351a1c3d3e1ac0974b28e6a1
detondev <50115345+DetonDev@users.noreply.github.com>
2020-02-13 09:58:37 -0500

    Make a few modifications

--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
-function type(txt) {
+function type(selectedElement, typeText) {
 
-  let i = 0;
-  txt.toString();
+  let i = 0; /* set i to 0 beforehand */
 
-  if (i < txt.length) {
+  if (i < typeText.length) {
 
-    let speed = Math.floor(Math.random() * (70 - 30 + 1) + 30);
-    document.getElementById("title").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(type, speed);
+    let speed = Math.floor(Math.random() * (70 - 30 + 1) + 30); /* vary the typing time between each letter */
+    document.getElementById(selectedElement).innerHTML += typeText.charAt(i); /* add a letter of the text to the selected element */
+    i++; /* increment to next letter */
+    setTimeout(type, speed); /* wait a few miliseconds before typing again */
 
   };
 
 };
 
-type("Samuel Giles");
+type("title", "Samuel Giles");
